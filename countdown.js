9ff9bd42b1bae2f75e0e738d091783dfab258242
vsaravi11 <vsaravi11@gmail.com>
2024-08-07 13:09:49 -0400

    Updated countdown target-date to September 5, 2024, at 8:20 PM
    Changed the countdown target date to reflect the new event date and time.

--- countdown.js
+++ countdown.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+  // Set your target date and time here
+  var countDownDate = new Date("Sep 5, 2024 20:20:00").getTime();
 
   var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
+    // Calculate days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // Display the result in the element with id="countdown"
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
       minutes + "m " + seconds + "s ";
 
+    // If the countdown is finished, display "EXPIRED"
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("countdown").innerHTML = "EXPIRED";
     }
   }, 1000);
 });
+
